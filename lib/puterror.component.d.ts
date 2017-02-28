76db0a76c600d30fe93c91e171a05c7e21d6fa81
jinlongzheng <jinlongzheng@skspruce.net>
2017-02-28 12:43:37 +0800

    clear data

--- lib/puterror.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { Renderer } from '@angular/core';
-export declare class PutErrorComponent {
-    private renderer;
-    control: any;
-    text?: string;
-    position?: string;
-    constructor(renderer: Renderer);
-}
--- lib/switch.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from '@angular/core';
-export declare class SwitchComponent {
-    onOff: boolean;
-    onOffChange: EventEmitter<boolean>;
-    readonly: boolean;
-    onChange: EventEmitter<boolean>;
-    constructor();
-    toggle: () => void;
-}
